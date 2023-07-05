5e8c727097a82b7f37173541582e421ba25c0c27
Satish Addanki <satishaddanki321@gmail.com>
2023-07-05 17:17:42 +0530

    satish

---
+++ src/TemplateCode/Cleaning/POS/Booking/BranchWise/OrderWithoutCheckout/AddListeners/OrderInfo/CheckOutButton/LocalToUrl.js
@@ -0,0 +1,12 @@
+let StartFunc = ({ inBranchName, inOrderNumber }) => {
+    let jVarLocalToUrl = "/public/Html/Cleaning/POS/Booking/Tabs/Settlement/Settlement.html";
+    const myUrlWithParams = new URL(`${window.location.origin}${jVarLocalToUrl}`);
+
+    myUrlWithParams.searchParams.append("BranchName", inBranchName);
+    myUrlWithParams.searchParams.append("OrderNumber", inOrderNumber);
+    window.location.href = myUrlWithParams.href;
+
+    // window.location.href = myUrlWithParams.href.replace("/AddItem/AddItemCommon", "/Settlement/Settlement");
+};
+
+export { StartFunc }
