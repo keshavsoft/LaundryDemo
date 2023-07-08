3ad95878021832a0abf9c9421473a7e3b3c8c379
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-07-08 20:27:08 +0530

    keshav

--- src/TemplateCode/Cleaning/POS/Booking/Tabs/SettlementWithDiscount/Js/FormLoad/ListenerFuncs/DiscountPerId/onkeyupAssign.js
+++ src/TemplateCode/Cleaning/POS/Booking/Tabs/SettlementWithDiscount/Js/FormLoad/ListenerFuncs/DiscountPerId/onkeyupAssign.js
@@ -18,8 +18,74 @@ function saveInput() {
     let jVarLocalOrderAmount = jFLocalFromDomOrderAmountId();
     let jVarLocalDiscountAmountId = document.getElementById("DiscountAmountId");
 
+    jVarLocalDiscountAmountId.value = (jVarLocalOrderAmount * ((100 - jVarLocalShowButtonClass.value) / 100)).toFixed(2);
 
-    console.log('Saving data : ', jVarLocalOrderAmount, jVarLocalShowButtonClass.value);
+    jFLocalCgstAmountId({ inCgstAmountId: ((jVarLocalOrderAmount - jVarLocalDiscountAmountId.value) * 5 / 100).toFixed(2) });
+    jFLocalSgstAmountId({ inSgstAmountId: ((jVarLocalOrderAmount - jVarLocalDiscountAmountId.value) * 5 / 100).toFixed(2) });
+    jFShowNettAmount();
+};
+
+let jFShowNettAmount = () => {
+    let jVarLocalAfterDiscount = jFLocalFromDomDiscountAmountId();
+    let jVarLocalSgstAmountId = jFLocalFromDomSgstAmountId();
+    let jVarLocalCgstAmountId = jFLocalFromDomCgstAmountId();
+
+    jFLocalNettAmountId({ inNettAmountId: parseFloat(jVarLocalAfterDiscount) + parseFloat(jVarLocalSgstAmountId) + parseFloat(jVarLocalCgstAmountId) });
+    let jVarLocalNettAmount = parseFloat(jFLocalFromDomNettAmountId());
+
+    jFLocalToInputRoundOffId({ inRoundOffId: (Math.round(jVarLocalNettAmount) - jVarLocalNettAmount).toFixed(2) });
+};
+
+let jFLocalFromDomNettAmountId = () => {
+    let jVarLocalHtmlNettAmountId = 'NettAmountId';
+    let jVarHtmlNettAmountId = document.getElementById(jVarLocalHtmlNettAmountId);
+    let jVarHtmlNettAmountIdValue = jVarHtmlNettAmountId.value.trim();
+    return jVarHtmlNettAmountIdValue;
+};
+
+let jFLocalToInputRoundOffId = ({ inRoundOffId }) => {
+    let jVarLocalHtmlId = 'RoundOffId';
+    let jVarLocalRoundOffId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalRoundOffId.value = inRoundOffId;
+};
+
+let jFLocalFromDomSgstAmountId = () => {
+    let jVarLocalHtmlSgstAmountId = 'SgstAmountId';
+    let jVarHtmlSgstAmountId = document.getElementById(jVarLocalHtmlSgstAmountId);
+    let jVarHtmlSgstAmountIdValue = jVarHtmlSgstAmountId.value.trim();
+    return jVarHtmlSgstAmountIdValue;
+};
+
+let jFLocalFromDomCgstAmountId = () => {
+    let jVarLocalHtmlCgstAmountId = 'CgstAmountId';
+    let jVarHtmlCgstAmountId = document.getElementById(jVarLocalHtmlCgstAmountId);
+    let jVarHtmlCgstAmountIdValue = jVarHtmlCgstAmountId.value.trim();
+    return jVarHtmlCgstAmountIdValue;
+};
+
+let jFLocalFromDomDiscountAmountId = () => {
+    let jVarLocalHtmlDiscountAmountId = 'DiscountAmountId';
+    let jVarHtmlDiscountAmountId = document.getElementById(jVarLocalHtmlDiscountAmountId);
+    let jVarHtmlDiscountAmountIdValue = jVarHtmlDiscountAmountId.value.trim();
+    return jVarHtmlDiscountAmountIdValue;
+};
+
+let jFLocalNettAmountId = ({ inNettAmountId }) => {
+    let jVarLocalHtmlId = 'NettAmountId';
+    let jVarLocalNettAmountId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalNettAmountId.value = inNettAmountId;
+};
+
+let jFLocalSgstAmountId = ({ inSgstAmountId }) => {
+    let jVarLocalHtmlId = 'SgstAmountId';
+    let jVarLocalSgstAmountId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalSgstAmountId.value = inSgstAmountId;
+};
+
+let jFLocalCgstAmountId = ({ inCgstAmountId }) => {
+    let jVarLocalHtmlId = 'CgstAmountId';
+    let jVarLocalCgstAmountId = document.getElementById(jVarLocalHtmlId);
+    jVarLocalCgstAmountId.value = inCgstAmountId;
 };
 
 let jFLocalFromDomOrderAmountId = () => {
