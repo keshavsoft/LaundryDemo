ca304d985bd01d9e8a7996dcdd2f160f427f0494
Satish Addanki <satishaddanki321@gmail.com>
2023-07-08 18:43:16 +0530

    satish

--- src/TemplateCode/Cleaning/POS/Booking/BranchWise/OrderWithoutCheckout/AddListeners/OrderInfo/CheckOutButton/ButtonClickFunc.js
+++ src/TemplateCode/Cleaning/POS/Booking/BranchWise/OrderWithoutCheckout/AddListeners/OrderInfo/CheckOutButton/ButtonClickFunc.js
@@ -1,21 +1,48 @@
-import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../../../ToLocalStorage/OrdersData/ItemsInOrder.js";
-
+import { StartFunc as StartFuncOrdersData } from "../../../../../../../../../../ToLocalStorage/OrdersData/Single.js";
 import { StartFunc as StartFuncPrepareData } from "./../PrepareData/StartFunc.js";
-import { StartFunc as StartFuncLocalToUrl } from "./LocalToUrl.js";
+import { StartFunc as StartFuncToUrlWithDiscount } from "./ToUrlWithDiscount.js";
+import { StartFunc as StartFuncToUrlWithoutDiscount } from "./ToUrlWithoutDiscount.js";
+import { StartFunc as StartFuncItemsInOrder } from "../../../../../../../../../../FromLocalStorage/ItemsInOrder/Bulk.js";
+import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../../../ToLocalStorage/OrdersData/ItemsInOrder.js";
+// import { StartFunc as StartFuncToLocalStorage } from "../../../../../../../../../../ToLocalStorage/OrdersData/ItemsInOrder.js";
 
 const StartFunc = () => {
     let jVarLocalOrderNumberFromDom = jFLocalFromDomOrderNumberId();
-    let jVarLocalPresentOrderData = StartFuncPrepareData();
+    let jVarLocalToLocalStorage = StartFuncPrepareData();
+    //let jVarLocalOrderNumber = StartFuncOrdersData({ inEntry: jVarLocalToLocalStorage });
     let jVarLocalOrderNumber = StartFuncToLocalStorage({
-        inEntry: jVarLocalPresentOrderData,
+        inEntry: jVarLocalToLocalStorage,
         inPk: jVarLocalOrderNumberFromDom
     });
     if (jVarLocalOrderNumber > 0) {
-        StartFuncLocalToUrl({
-            inBranchName: jVarLocalPresentOrderData.JsonData.OrderData.BranchName,
-            inOrderNumber: jVarLocalOrderNumber
-        });
+        let jVarLocalIsDiscountGiven = jFlocalIsDiscountGiven();
+
+        if (jVarLocalIsDiscountGiven === true) {
+            StartFuncToUrlWithoutDiscount({
+                //  inBranchName: jVarLocalToLocalStorage.CustomerData.BranchName,
+                inBranchName: "Hello",
+                inOrderNumber: jVarLocalOrderNumber
+            });
+        }
+        else {
+            StartFuncToUrlWithDiscount({
+                // inBranchName: jVarLocalToLocalStorage.CustomerData.BranchName,
+                inBranchName: "Hello",
+                inOrderNumber: jVarLocalOrderNumber
+            });
+        }
     };
+};
+let jFlocalIsDiscountGiven = () => {
+
+    let jVarLocalItemData = StartFuncItemsInOrder();
+    let jVarLocalFiltered = Object.values(jVarLocalItemData).filter(element => {
+        return element.DisPer > 0;
+    });
+
+    if (jVarLocalFiltered.length > 0) return true;
+    return false;
+
 };
 
 let jFLocalFromDomOrderNumberId = () => {
